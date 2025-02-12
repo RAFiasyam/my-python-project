a4f9a41c42553b2c61648086758841a691bae167
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-12 12:48:16 +0700

    variable name

---
+++ src/main.py
@@ -0,0 +1,14 @@
+# Variable Names (A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume).)
+myvar = "rafi"
+my_var = "rafi"
+_my_var = "rafi"
+myVar = "rafi"
+MYVAR = "rafi"
+myvar2 = "rafi"
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
