1fc16f4d14fee054a10199afbb49937d7262f3ed
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-12 12:39:21 +0700

    adding half of this page

--- src/main.py
+++ src/main.py
@@ -1,12 +1,24 @@
-# this is python syntax short explain
-
-# python indentation
-if 5 > 2:
-    print("Five is greater then two!")
-
-# python variable
+# Creating variable
 x = 5
-y = "hello"
+y = "bakwan"
+print(x)
+print(y)
 
-print(f"{x}")
-print(f"{y}")
+
+# casting (If you want to specify the data type of a variable, this can be done with casting.)
+a = str(3) # a will be '3'
+b = int(3) # b will be 3
+c = float(3) # c will be 3.0
+print(a, b, c)
+
+# get the type
+aa = 5
+bb = "rafi"
+print(type(aa))
+print(type(bb))
+
+# single or double quote are same
+x = "rafi"
+# is the same as
+x = 'rafi'
+
