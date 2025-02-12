ef85aa179759e41e9d80a617eb81c40377534060
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-12 12:18:51 +0700

    first commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+print("Hello everyone!")
+
+name = "Rafi asyam"
+username = "Rafi"
+age = 20
+print(f"im {name}, my age {age}")
+
+def greet(nama) :
+    return f"hello {username}, you can learn python here"
+
+print(greet("Rafi asyam"))
